e3a7ba69063df8e33e2341a6684f4994751655ab
Navy881 <ag881.pst@gmail.com>
2025-04-03 02:53:07 +0300

    update retry logic

--- sweet_cash/integrations/base_integration.py
+++ sweet_cash/integrations/base_integration.py
@@ -1,5 +1,7 @@
 
 import asyncio
+import logging
+
 import aiohttp
 from aiohttp_retry import RetryClient, ExponentialRetry
 from pydantic import AnyHttpUrl, ValidationError
@@ -9,34 +11,43 @@ from urllib.parse import urljoin
 from sweet_cash.errors import APIError
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
+
 class BaseIntegration(object):
 
     def __init__(self, session: aiohttp.ClientSession, timeout: float, url: AnyHttpUrl):
-        retry_options = ExponentialRetry(attempts=3, start_timeout=1, max_timeout=10)
-        self.session = RetryClient(session, retry_options=retry_options)
+        retry_options = ExponentialRetry(
+            attempts=5,
+            start_timeout=1,
+            max_timeout=15,
+            statuses={400, 401, 403, 404, 429, 498, 500, 502, 503, 504},
+            methods={"GET", "POST"},
+            exceptions={Exception}
+        )
+        self.session = RetryClient(session, retry_options=retry_options, logger=logger)
         self.timeout = aiohttp.ClientTimeout(timeout)
         self.url = url
-
-    # def _check_error(resp_json_body: Dict[str, Any], status_code: int) -> None:
-    #     if status_code < 400:
-    #         return
-    #     raise APIError(message=str(resp_json_body), status_code=status_code)
 
     async def _request(self, method: str, url: AnyStr, **kwargs: Any) -> Union[Dict[str, Any], List[Any]]:
         try:
             async with self.session.request(
                 method=method, url=urljoin(self.url, url), timeout=self.timeout, ssl=False, **kwargs
             ) as resp:
-                if resp.status >= 400:
-                    resp_content = await resp.content.read()
-                    error_message = f'{self.url} error: {resp_content.decode("utf-8")}'
-                    raise APIError(message=error_message, status_code=resp.status)
-                resp_json_body = await resp.json(content_type=None)
-                # self._check_error(resp_json_body=resp_json_body, status_code=resp.status)
-                return cast(Dict[str, Any], resp_json_body)
+                resp.raise_for_status()
+
+                try:
+                    resp_json_body = await resp.json(content_type=None)
+                    return cast(Dict[str, Any], resp_json_body)
+                except Exception as exc:
+                    raise APIError(message=str(exc))
 
         except (asyncio.TimeoutError, aiohttp.ClientError, ValidationError, ConnectionError) as exc:
-            raise APIError(exc)
+            raise APIError(message=str(exc))
 
 
 # class BaseIntegration(object):
